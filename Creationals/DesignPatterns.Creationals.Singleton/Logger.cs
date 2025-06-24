431524437e2416861a91fd1caa527aef7d5dbdb7
Mateusz_Górny <59448497+Mefju08@users.noreply.github.com>
2025-06-24 20:21:39 +0200

    add singleton

---
+++ Creationals/DesignPatterns.Creationals.Singleton/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creationals.Singleton
+{
+    internal sealed class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new(() => new Logger());
+        private Logger() { }
+
+        public static Logger Instance => _instance.Value;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+        }
+    }
+}
---
+++ Creationals/DesignPatterns.Creationals.Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Creationals.Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = Logger.Instance;
+            logger.Log("Everithing is working");
+        }
+    }
+}
