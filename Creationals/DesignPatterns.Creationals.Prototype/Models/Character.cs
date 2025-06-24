1a75916b5eff17c4149f07964bba7d66801b5d4d
Mateusz_Górny <59448497+Mefju08@users.noreply.github.com>
2025-06-24 20:39:13 +0200

    add prototype

---
+++ Creationals/DesignPatterns.Creationals.Prototype/Models/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creationals.Prototype.Models
+{
+    internal sealed class Character
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+        public List<string> Skills { get; set; }
+
+        public Character(string name, string @class, int health, int mana, List<string> skills)
+        {
+            Id = Guid.CreateVersion7();
+            Name = name;
+            Class = @class;
+            Health = health;
+            Mana = mana;
+            Skills = skills;
+        }
+        public Character Clone()
+        {
+            var character = (Character)this.MemberwiseClone();
+            character.Id = Guid.CreateVersion7();
+            character.Skills = new List<string>( Skills);
+            
+            return character;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Id: {Id} | [{Class}] {Name} | HP: {Health}, Mana: {Mana}");
+            Console.WriteLine("Skills: " + string.Join(", ", Skills));
+            Console.WriteLine(  );
+        }
+    }
+}
---
+++ Creationals/DesignPatterns.Creationals.Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatterns.Creationals.Prototype.Models;
+
+namespace DesignPatterns.Creationals.Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var character = new Character("Edd", "Warrior", 100, 
+                200, new List<string>()
+                {
+                   "attack"
+                });
+            character.Display();
+
+            var characterCopy = character.Clone();
+            characterCopy.Display();
+        }
+    }
+}
